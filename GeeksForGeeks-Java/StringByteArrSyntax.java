7c0782bc97abe5bae35426d515484bdac07e9832
Alfredo C <alfredo_cardenas@live.com>
2025-03-24 22:53:56 -0600

    Add notes on Strings, String Immutability, and an example

---
+++ GeeksForGeeks-Java/StringByteArrSyntax.java
@@ -0,0 +1,19 @@
+// Construct String from subset of char array
+
+// Driver Class
+class GFG {
+  public static void main(String args[]) {
+        
+        // Creating Byte ASCII Array
+      byte ascii[] = { 71, 70, 71 };
+
+        // Creating String using byte array
+      String firstString = new String(ascii);
+      System.out.println(firstString);
+
+        // Creating String using byte array with
+        // Start index to End Index
+      String secondString = new String(ascii, 1, 2);
+      System.out.println(secondString);
+  }
+}
