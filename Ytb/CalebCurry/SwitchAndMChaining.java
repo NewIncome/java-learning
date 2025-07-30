49b04aa4291d31794de0a9cfd7a914b2e9f3ba0c
Alfredo C <alfredo_cardenas@live.com>
2025-07-30 13:53:47 -0600

    Add file for Method Chaining and Switch statement

---
+++ Ytb/CalebCurry/SwitchAndMChaining.java
@@ -0,0 +1,34 @@
+package Ytb.CalebCurry;
+
+import java.util.Scanner;
+
+public class SwitchAndMChaining {
+  public static void main(String[] args) {
+    //Switch statment exercise
+    System.out.println("What's your name?: ");
+
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+
+    switch(name) {
+      case "Caleb":
+        System.out.println("Welcome my man");
+        break;
+      case "Clare":
+        System.out.println("Pretty girls, ofcourse");
+        break;
+      case "Jorel":
+      case "Kalel":
+        System.out.println("No cheating with superpowers");
+        break;
+      default:
+        System.out.println("Try again later");
+        break;
+    }
+
+    boolean welcome = false;
+    //Ternary Operator, and Method Chaining
+    if(name.toLowerCase().equals(name)) welcome = true;
+    System.out.println(welcome);
+  }
+}
