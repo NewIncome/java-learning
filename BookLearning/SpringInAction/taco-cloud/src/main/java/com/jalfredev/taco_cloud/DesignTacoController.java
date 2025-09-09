cf78a84eeb8986747f3da453fea5953102d6ca7b
Alfredo C <alfredo_cardenas@live.com>
2025-09-09 16:28:22 -0600

    Modify DesignTaco controller, due to bad code in eBook - corrections from author github page: https://github.com/habuma/spring-in-action-5-samples

--- BookLearning/SpringInAction/taco-cloud/src/main/java/com/jalfredev/taco_cloud/DesignTacoController.java
+++ BookLearning/SpringInAction/taco-cloud/src/main/java/com/jalfredev/taco_cloud/DesignTacoController.java
@@ -2,28 +2,29 @@ package com.jalfredev.taco_cloud;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jalfredev.taco_cloud.Ingredient.Type;
+import com.jalfredev.taco_cloud.Ingredient;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Slf4j          //Generates a Logger in the class
 @Controller     //Mark the class as a candidate for component scanning
 @RequestMapping("/design")  //Map web requests to specific methods in a controller class
 public class DesignTacoController {
-
-  /* @GetMapping Specifies that when an HTTP GET request is received for the page specified
-   * in the @RequestMapping, the following method will be called to handle the request
+ 
+  /* Model is an object that ferries data between a controller and whatever view is charged with rendering that data
+   * 
    */
-  @GetMapping
-  public String showDesignForm(Model model) {
-    /* Model is an object that ferries data between a controller and whatever view is charged with rendering that data */
-
+  @ModelAttribute
+  public void addIngredientsToModel(Model model) {
     //hardcoded list of ingredients, for now...
     List<Ingredient> ingredients = Arrays.asList(
       new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
@@ -37,18 +38,31 @@ public class DesignTacoController {
       new Ingredient("SLSA", "Salsa", Type.SAUCE),
       new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
     );
-
+    
     //filters the list by ingredient type
     Type[] types = Ingredient.Type.values();
-    for(Type type : types) {
+    for (Type type : types) {
       model.addAttribute(type.toString().toLowerCase(),
-        filterByType(ingredients, type));
+          filterByType(ingredients, type));
     }
+  }
 
+  /* @GetMapping Specifies that when an HTTP GET request is received for the page specified
+   * in the @RequestMapping, the following method will be called to handle the request
+   */
+  @GetMapping
+  public String showDesignForm(Model model) {
     model.addAttribute("design", new Taco());
-
     //the logical name of the view that will be used to render the model to the browser
     return "design";
   }
-  
+
+  private List<Ingredient> filterByType(List<Ingredient> ingredients,
+                                        Type type) {
+      return ingredients
+              .stream()
+              .filter(x -> x.getType().equals(type))
+              .collect(Collectors.toList());
+  }
+
 }
