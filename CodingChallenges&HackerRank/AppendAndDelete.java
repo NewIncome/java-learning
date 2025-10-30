adb7e685a4020a20a52df0de1fdd9f16d10c8ad3
Alfredo C <alfredo_cardenas@live.com>
2025-10-30 09:18:21 -0600

    bad intent on trying to solve AppendAndDelete cc

--- CodingChallenges&HackerRank/AppendAndDelete.java
+++ CodingChallenges&HackerRank/AppendAndDelete.java
@@ -38,13 +38,60 @@
 public class AppendAndDelete {
 
   public static void main(String[] args) {
-    System.out.println(appendAndDelete("abc", "def", 6)); //Yes
-    System.out.println(appendAndDelete("hackerhappy", "hackerrank", 9));
-    System.out.println(appendAndDelete("aba", "aba", 7));
-    System.out.println(appendAndDelete("ashley", "ash", 2));
+    System.out.println(solution03("abc", "def", 6)); //Yes
+    System.out.println(solution03("hackerhappy", "hackerrank", 9));
+    System.out.println(solution03("aba", "aba", 7)); //Yes
+    System.out.println(solution03("ashley", "ash", 2)); //No
   }//end main
 
-  public static String appendAndDelete(String s, String t, int k) {
+  //having the same issue/error/bad answer, as in solution02()
+  public static String solution03(String s, String t, int k) {
+    int count = 0;
+    char[] sArr = s.toCharArray();
+    char[] tArr = t.toCharArray();
+    System.out.println("sArr[i] == tArr[i]");
+    for (int i = 0; i < t.length(); i++) {
+      System.out.println("i: " + i + ", " + sArr[i] + " == " + tArr[i]);
+      if (sArr[i] == tArr[i]) count+=1;
+    }
+    int tMinus = t.length();
+    int sMinus = s.length();
+    int stk = k - (tMinus + sMinus);
+
+    if(tMinus+sMinus < k && t.length()+s.length() > k && stk%2!=0) return "No";
+    else if (tMinus+sMinus <= k) return "Yes";
+    else return "No";
+  }//end solution03
+
+  //Solution from Python code, not working
+  public static String solution02(String s, String t, int k) {
+    int count = 0;
+
+    for(char i : s.toCharArray()) {
+      for(char j : t.toCharArray()) {
+        if (i == j) ++count;
+        else break;
+      }
+    }
+
+    int totalL = s.length() + t.length();
+
+    /*  Case 1
+        totalL <= ((2*count) + k)
+        Case 2
+        totalL%2 == k%2 : "Yes"
+        Case 3
+        totalL < k
+        case 1 and case 2 or case 3 */
+
+        if(((totalL <= ((2*count) + k)) && (totalL%2 == k%2)) || totalL<k) {
+          return "Yes";
+        } else {
+          return "No";
+        }
+  }//end solution02
+
+  public static String solution01(String s, String t, int k) {
     if(s == t) return "Yes";
     if((s.length() + t.length()) <= k) return "Yes";
     if(Math.abs(s.length() - t.length()) > k) return "No";
@@ -61,6 +108,6 @@ public class AppendAndDelete {
     }
 
     return "No";
-  }//end appendAndDelete
+  }//end solution01
 
 }
