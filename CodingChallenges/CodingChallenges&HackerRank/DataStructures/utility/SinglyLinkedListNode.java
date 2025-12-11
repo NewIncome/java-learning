8c0d2cede9d9772ca0bf4eb0aa283d61088bbaa4
Alfredo C <alfredo_cardenas@live.com>
2025-12-11 10:16:35 -0600

    feat: create a utility package for SinglyLinked List and Node classes for HR CC

---
+++ CodingChallenges/CodingChallenges&HackerRank/DataStructures/utility/SinglyLinkedListNode.java
@@ -0,0 +1,14 @@
+/**
+ * Node class to create Nodes for HackerRank coding challenges
+ */
+package utility;
+
+public class SinglyLinkedListNode {
+    public int data;
+    public SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int nodeData) {
+      this.data = nodeData;
+      this.next = null;
+    }
+  }
