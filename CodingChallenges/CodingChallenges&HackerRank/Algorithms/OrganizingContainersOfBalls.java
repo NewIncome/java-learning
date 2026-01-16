89474e577b1493fd1344b879bc31221834e6b7a2
Alfredo C <alfredo_cardenas@live.com>
2026-01-16 14:40:59 -0600

    feat: add Alg-CC for OrganizingContainersOfBalls - had an understaing issue, assumed containers where fixed for a ballType

--- CodingChallenges/CodingChallenges&HackerRank/Algorithms/OrganizingContainersOfBalls.java
+++ CodingChallenges/CodingChallenges&HackerRank/Algorithms/OrganizingContainersOfBalls.java
@@ -53,21 +53,140 @@
  *         19     >       3      +  14    (= 17)      //-TC3  (X)
  *     *    8     <       3      +   7    (= 10)      //-TC4  (✔)
  */
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class OrganizingContainersOfBalls {
 
   public static void main(String[] args) {
+    //double startTimeMillis = System.currentTimeMillis();
+    double startTimeNano = System.nanoTime();
+
+
     System.out.println(organizingContainers(List.of( List.of(1,4), List.of(2,3) )));  //Impossible
     System.out.println(organizingContainers(List.of( List.of(1,1), List.of(1,1) )));  //Possible
     System.out.println(organizingContainers(List.of( List.of(0,2), List.of(1,1) )));  //Impossible
     System.out.println(organizingContainers(List.of( List.of(1,3,1), List.of(2,1,2), List.of(3,3,3) )));  //Impossible
     System.out.println(organizingContainers(List.of( List.of(0,2,1), List.of(1,1,1), List.of(2,0,0) )));  //Possible
+
+
+    //double endTimeMillis = System.currentTimeMillis();
+    double endTimeNano = System.nanoTime();
+    //double totalMillis = endTimeMillis - startTimeMillis;
+    double totalNano = endTimeNano - startTimeNano;
+    //System.err.printf("Total time elapsed: %.2f milliS :: %.4f seconds%n", totalMillis, (totalMillis/1_000));
+    System.err.printf("Total time elapsed: %.2f nanoS :: %.3f seconds.", totalNano, (totalNano/1_000_000_000));
+    System.err.println(" Mustn't be greater than 2 seconds");
   }
 
+
+  /* My issue was thinking each container had to be fixed with a ballType,
+   * as if the containers also had types.
+   * Basically,
+   *  - there are ballTypes
+   *  - there are container sizes
+   * And, we just have to see if we can match containerSizes vs/per ballTypeCount
+   * Therefore, sorting is best used here
+   * 
+   *  >=>-> *** So MUST ASSUME, containers are not fixed with/for a specific ballType !!! <-<=<
+   */
   public static String organizingContainers(List<List<Integer>> container) {
+      int n = container.size();
+      int balls;
+
+      long[] containerSums = new long[n];
+      long[] typeSums = new long[n];
+
+      for (int i = 0; i < n; i++) {
+          for (int j = 0; j < n; j++) {
+              balls = container.get(i).get(j);
+              containerSums[i] += balls;  // row sum
+              typeSums[j] += balls;       // column sum
+          }
+      }
+
+      Arrays.sort(containerSums);
+      Arrays.sort(typeSums);
+
+      return Arrays.equals(containerSums, typeSums)
+              ? "Possible"
+              : "Impossible";
+  }
+
+
+  public static String organizingContainers03(List<List<Integer>> container) {
+    int n = container.size();
+    BigInteger contCapacity = BigInteger.ZERO;
+    BigInteger ballTypeCount = BigInteger.ZERO;
+
+    for (int i = 0; i < n; i++) {
+      contCapacity = BigInteger.valueOf(container.get(i).stream().reduce((a,b)->a+b).orElse(0));
+
+      for (int j = 0; j < n; j++) {
+        //System.out.println("j: " + j + ", BigInteger.valueOf(container.get(j).get(i)): " + BigInteger.valueOf(container.get(j).get(i)) + ", ballTypeCount: " + ballTypeCount);
+        ballTypeCount = ballTypeCount.add(BigInteger.valueOf(container.get(j).get(i)));
+      }
+
+      //System.out.println("i: " + i + ", containerCount: " + contCapacity + ", ballTypeCount: " + ballTypeCount);
+      if(contCapacity.compareTo(ballTypeCount) != 0) return "Impossible";
+      ballTypeCount = BigInteger.ZERO;
+      contCapacity = BigInteger.ZERO;
+    }
+
+    return "Possible";
+  }
+
+  public static String organizingContainers02(List<List<Integer>> container) {
+    //int val;
+    int n = container.size();
+    long[] contCapacity = new long[n];
+    long[] ballTypeCount = new long[n];
+
+    //fill sums of: row & col, contCapacity & ballTypeCount
+    /* for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        val = container.get(i).get(j);
+        contCapacity[i] += val;
+        ballTypeCount[j] += val;
+      }
+    } */
+    for (int i = 0; i < n; i++) {
+      contCapacity[i] = container.get(i).stream().reduce((a,b)->a+b).orElse(0);
+      //contCapacity[i] = container.get(i).stream().mapToInt(Integer::intValue).sum();
+      for (int j = 0; j < n; j++) {
+        ballTypeCount[i] += container.get(j).get(i);
+      }
+      //System.out.println("i: " + i + ", containerCount: " + contCapacity[i] + ", ballTypeCount: " + ballTypeCount[i]);
+      if(contCapacity[i] != ballTypeCount[i]) return "Impossible";
+    }
+
+    //compare
+    /* for (int i = 0; i < n; i++) {
+      if(contCapacity[i] != ballTypeCount[i]) return "Impossible";
+    } */
+
+    //can i make a loop that sums and compares row by col?
+
+    return "Possible";
+  }
+  /* if done the sum separately:
+    • add the Rows
+       List<Integer> rowSums = matrix.stream()
+          .map(row -> row.stream().mapToInt(Integer::intValue).sum())
+          .toList();
+    • add the Cols
+       List<Integer> colSums = IntStream.range(0, numCols)
+          .mapToObj(colIdx -> matrix.stream()
+              .mapToInt(row -> row.get(colIdx))
+              .sum())
+          .toList();
+   */
+
+
+  public static String organizingContainers01(List<List<Integer>> container) {
     // I need to get:  TotalBalls, TotalBallsNotInPlace, TotalContainers
     int totalBalls = 0;
     int totalBallsNotInPlace = 0;
