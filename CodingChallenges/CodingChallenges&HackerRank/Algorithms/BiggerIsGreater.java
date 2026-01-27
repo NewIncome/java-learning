7ec052571de8978dd434edbff54f13c8416f720b
Alfredo C <alfredo_cardenas@live.com>
2026-01-27 12:33:13 -0600

    fix: add solution to Alg-CC BiggerIsGreater

--- CodingChallenges/CodingChallenges&HackerRank/Algorithms/BiggerIsGreater.java
+++ CodingChallenges/CodingChallenges&HackerRank/Algorithms/BiggerIsGreater.java
@@ -57,7 +57,16 @@
   ۝ remove NextMaxChar from W
   ۝ descendingSort W
   ۝ return nextMaxChar+W
+
+  wVid
+   1• Traverse from Right-to-Left. Because we need to swap the to-right char with the nearestBigLeft
+   2• Find the lower char. the elm that's lower than the previous elm
+   3• Separate the String, get the Left-side, after the 1stLower
+   4• Find the min-char in that sub-string after 1stLower that is also greater than 1stLower
+   5• Swap that min-char-subString(2ndLower) with 1stLower
+   6• Now Sort that substring after 2ndLower, in ascending order
  */
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 
@@ -67,8 +76,77 @@ public class BiggerIsGreater {
     System.out.println(biggerIsGreater("ab"));
   }
 
+
   public static String biggerIsGreater(String w) {
-    if(w.length() == 1) return "no answer";
+    char charArray[] = w.toCharArray();
+    int n = charArray.length;
+    int endIndex = 0;
+
+    for (endIndex = n-1; endIndex > 0; endIndex--) {
+      if(charArray[endIndex] > charArray[endIndex - 1]) break;
+    }
+
+    if(endIndex == 0) return "no answer";
+    else {
+      int firstSmallChar = charArray[endIndex - 1];
+      int nextSmallChar = endIndex;
+
+      for (int startIndex = endIndex + 1; startIndex < n; startIndex++) {
+        if(charArray[startIndex] > firstSmallChar && charArray[startIndex] < charArray[nextSmallChar]) {
+          nextSmallChar = startIndex;
+        }
+      }
+
+      swap(charArray, endIndex - 1, nextSmallChar);
+
+      Arrays.sort(charArray, endIndex, n);
+    }
+
+    return new String(charArray);
+  }
+  static void swap(char charArray[], int i, int j) {
+    char temp = charArray[i];
+    charArray[i] = charArray[j];
+    charArray[j] = temp;
+  }
+
+  public static String biggerIsGreater02(String w) {
+    char[] wArr = w.toCharArray();
+    char[] subW;
+    int firstLowerIdx = 0;
+    int secondLowerIdx = 0;
+
+    //1.Traverse string from L-R to find 1stLower
+    for (int i = wArr.length-1; i >= 1; i--) {
+      if(wArr[i-1] < wArr[i]) firstLowerIdx = i;  //2
+    }
+
+    //In case it didn't find any char lower than the first char
+    if(firstLowerIdx == 0) return "no answer";
+
+    //3.Separate the string
+    subW = w.substring(firstLowerIdx, w.length()).toCharArray();
+    wArr = w.substring(0, firstLowerIdx).toCharArray();
+
+    //4.Traverse sub-string to find 2ndLower & > 1stLower
+    for (int i = 0; i > subW.length; i++) {
+      if(subW[i] < subW[i+1] && subW[i] > wArr[firstLowerIdx]) secondLowerIdx = i;
+    }
+
+    //5.Swap min-char-subString(2ndLower) with 1stLower
+    char toSwap = w.charAt(firstLowerIdx);
+    wArr[firstLowerIdx] = subW[secondLowerIdx];
+    //wArr[secondLowerIdx] = toSwap;
+    subW[secondLowerIdx] = toSwap;
+
+    //6.Sort substring
+    Arrays.sort(subW);
+
+    return String.valueOf(wArr) + String.valueOf(subW);
+  }
+
+  public static String biggerIsGreater01(String w) {
+    /* if(w.length() == 1) return "no answer";
     if(w.length() == 2) {
       if(w.charAt(0) >= w.charAt(1)) return "no answer";
       else return (String.valueOf(w.charAt(0)) + String.valueOf(w.charAt(1)));
@@ -83,7 +161,7 @@ public class BiggerIsGreater {
 
       }
     }
-    
+    */
     return "";
   }
 
